380b41568a7e8fee425984afb3a434905c94dde3
Salvarez08 <salvarez565@soyudemedellin.edu.co>
2026-02-10 11:49:09 -0500

    lo mismo que el pasado pero ahora me saluda a mi
    unity 2023

--- Mi Primera Cabalgata 1/Assets/_Main/Scripts/Variables.cs
+++ Mi Primera Cabalgata 1/Assets/_Main/Scripts/Variables.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Declarar Variables
+
+    string myName = "Salomón";
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log ("hola mundo start() !!");
+        Debug.Log ("hola " + myName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hola mundo update");
+     
     }
 }
